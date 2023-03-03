4b4284ecc494a1491d4839bab15542a499093db6
Mika <mikavandenbrink@outlook.com>
2023-03-03 13:04:09 +0100

    Added all DbSets

--- SimTECH/SimTECH/Data/SimTechDbContext.cs
+++ SimTECH/SimTECH/Data/SimTechDbContext.cs
@@ -10,6 +10,32 @@ namespace SimTECH.Data
         {
         }
 
+        public DbSet<Contract> Contracts => Set<Contract>();
+        public DbSet<DevelopmentRange> DevelopmentRanges => Set<DevelopmentRange>();
+        public DbSet<Driver> Drivers => Set<Driver>();
+        public DbSet<DriverTrait> DriverTraits => Set<DriverTrait>();
+        public DbSet<Engine> Engines => Set<Engine>();
+        public DbSet<League> Leagues => Set<League>();
+        public DbSet<Manufacturer> Manufacturers => Set<Manufacturer>();
+        public DbSet<Penalty> Penaltys => Set<Penalty>();
+        public DbSet<PointAllotment> PointAllotments => Set<PointAllotment>();
+        public DbSet<Race> Races => Set<Race>();
+        public DbSet<Result> Results => Set<Result>();
+        public DbSet<Season> Seasons => Set<Season>();
+        public DbSet<SeasonDriver> SeasonDrivers => Set<SeasonDriver>();
+        public DbSet<SeasonEngine> SeasonEngines => Set<SeasonEngine>();
+        public DbSet<SeasonTeam> SeasonTeams => Set<SeasonTeam>();
+        public DbSet<Sponsor> Sponsors => Set<Sponsor>();
+        public DbSet<Stint> Stints => Set<Stint>();
+        public DbSet<StintResult> StintResults => Set<StintResult>();
+        public DbSet<Strategy> Strategys => Set<Strategy>();
+        public DbSet<StrategyTyre> StrategyTyres => Set<StrategyTyre>();
+        public DbSet<Team> Teams => Set<Team>();
+        public DbSet<TeamTrait> TeamTraits => Set<TeamTrait>();
+        public DbSet<Track> Tracks => Set<Track>();
+        public DbSet<TrackTrait> TrackTraits => Set<TrackTrait>();
+        public DbSet<Trait> Traits => Set<Trait>();
+        public DbSet<Tyre> Tyres => Set<Tyre>();
         public DbSet<User> Users => Set<User>();
 
         protected override void OnModelCreating(ModelBuilder builder)
