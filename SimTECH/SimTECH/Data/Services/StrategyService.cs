6c364e12a40788795c6e839c94b394ddfa06a695
Mika <mikavandenbrink@outlook.com>
2023-03-03 16:44:48 +0100

    adding strategies works more properly now

--- SimTECH/SimTECH/Data/Services/StrategyService.cs
+++ SimTECH/SimTECH/Data/Services/StrategyService.cs
@@ -7,6 +7,43 @@ namespace SimTECH.Data.Services
     {
         private readonly IDbContextFactory<SimTechDbContext> _dbFactory;
 
+        private readonly List<Tyre> _tyres = new()
+        {
+            new Tyre()
+            {
+                Id = 1,
+                Name = "Hard",
+                Colour = "#e4e2e2ff",
+                State = State.Active,
+                Length = 9,
+                Pace = 10,
+                WearMax = -2,
+                WearMin = -1,
+            },
+            new Tyre()
+            {
+                Id = 2,
+                Name = "Medium",
+                Colour = "#e2a83eff",
+                State = State.Active,
+                Length = 6,
+                Pace = 15,
+                WearMax = -6,
+                WearMin = -3,
+            },
+            new Tyre()
+            {
+                Id = 3,
+                Name = "Soft",
+                Colour = "#d61616ff",
+                State = State.Active,
+                Length = 3,
+                Pace = 30,
+                WearMax = -15,
+                WearMin = -10,
+            },
+        };
+
         public StrategyService(IDbContextFactory<SimTechDbContext> factory)
         {
             _dbFactory = factory;
@@ -31,7 +68,9 @@ namespace SimTECH.Data.Services
         {
             using var context = _dbFactory.CreateDbContext();
 
-            return await context.Tyre.ToListAsync();
+            var tyres = await context.Tyre.ToListAsync();
+
+            return _tyres;
         }
 
         public async Task CreateTyre(Tyre tyre)
