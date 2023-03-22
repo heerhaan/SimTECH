4ed10336dd449d489e27c56f54bffdf0629b16f0
Mika <mikavandenbrink@outlook.com>
2023-03-22 09:50:31 +0100

    Can delete concept seasons now, wow!

--- SimTECH/SimTECH/Data/Services/SeasonService.cs
+++ SimTECH/SimTECH/Data/Services/SeasonService.cs
@@ -16,7 +16,9 @@ namespace SimTECH.Data.Services
         {
             using var context = _dbFactory.CreateDbContext();
 
-            return await context.Season.Include(e => e.PointAllotments).ToListAsync();
+            return await context.Season
+                .Include(e => e.PointAllotments)
+                .ToListAsync();
         }
 
         public async Task<Season> GetSeasonById(long seasonId)
@@ -38,6 +40,34 @@ namespace SimTECH.Data.Services
                 context.Update(season);
 
             await context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteSeason(Season? season)
+        {
+            using var context = _dbFactory.CreateDbContext();
+
+            if (season == null)
+                throw new InvalidOperationException("No season related to ID found");
+            if (season.State != State.Concept)
+                throw new InvalidOperationException("Can only delete seasons which are in concept");
+
+            var seasonDrivers = await context.SeasonDriver.Where(e => e.SeasonId == season.Id).ToListAsync();
+            var seasonTeams = await context.SeasonTeam.Where(e => e.SeasonId == season.Id).ToListAsync();
+            var seasonEngines = await context.SeasonEngine.Where(e => e.SeasonId == season.Id).ToListAsync();
+            var racesInSeason = await context.Race
+                .Include(e => e.Stints)
+                .Where(e => e.SeasonId == season.Id)
+                .ToListAsync();
+
+            context.RemoveRange(seasonDrivers);
+            context.RemoveRange(seasonTeams);
+            context.RemoveRange(seasonEngines);
+            context.RemoveRange(racesInSeason);
+            context.Remove(season);
+
+            await context.SaveChangesAsync();
+
+            return true;
         }
     }
 }
