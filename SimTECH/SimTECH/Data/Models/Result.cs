5af4faece5dc13a1c9051a0063f30947cafc4e94
Mika <mikavandenbrink@outlook.com>
2023-03-23 18:19:35 +0100

    Activation of race and making the model for race week

--- SimTECH/SimTECH/Data/Models/Result.cs
+++ SimTECH/SimTECH/Data/Models/Result.cs
@@ -20,6 +20,6 @@
         public long StrategyId { get; set; }
         public Strategy Strategy { get; set; } = default!;
 
-        public IList<StintResult> StintResults { get; set; } = default!;
+        public IList<StintResult>? StintResults { get; set; }
     }
 }
--- SimTECH/SimTECH/Data/Services/RaceService.cs
+++ SimTECH/SimTECH/Data/Services/RaceService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SimTECH.Data.Models;
+using SimTECH.Extensions;
 using SimTECH.PageModels;
 
 namespace SimTECH.Data.Services
@@ -23,6 +24,17 @@ namespace SimTECH.Data.Services
                 .ToListAsync();
         }
 
+        public async Task<Race> GetRaceById(long raceId)
+        {
+            using var context = _dbFactory.CreateDbContext();
+
+            return await context.Race
+                .Include(e => e.Track)
+                .Include(e => e.Stints)
+                .Include(e => e.Penalties)
+                .SingleAsync(e => e.Id == raceId);
+        }
+
         public async Task UpdateRace(Race race)
         {
             using var context = _dbFactory.CreateDbContext();
@@ -31,6 +43,52 @@ namespace SimTECH.Data.Services
                 context.Add(race);
             else
                 context.Update(race);
+
+            await context.SaveChangesAsync();
+        }
+
+        public async Task ActivateRace(long raceId)
+        {
+            using var context = _dbFactory.CreateDbContext();
+
+            var race = await context.Race
+                .Include(e => e.Stints)
+                .SingleAsync(e => e.Id == raceId);
+
+            if (race.State != State.Concept)
+                throw new InvalidOperationException("Can only activate races in concept state");
+
+            var seasonDrivers = await context.SeasonDriver
+                .Where(e => e.SeasonId == race.SeasonId && e.SeasonTeamId.HasValue)
+                .ToListAsync();
+
+            var strategiesForRace = await context.Strategy
+                .Include(e => e.StrategyTyres)
+                    .ThenInclude(e => e.Tyre)
+                .Where(e => e.StintLength == race.Stints.Count)
+                .ToListAsync();
+
+            if (strategiesForRace?.Any() != true)
+                throw new InvalidOperationException("No valid strategies for this race, smh");
+
+            var driverResults = new List<Result>();
+
+            foreach (var driver in seasonDrivers)
+            {
+                var strategy = strategiesForRace[NumberHelper.RandomInt(strategiesForRace.Count - 1)];
+
+                driverResults.Add(new Result
+                {
+                    Status = RaceStatus.Racing,
+                    TyreLife = strategy.StrategyTyres[0].Tyre.Pace,
+                    SeasonDriverId = driver.Id,
+                    SeasonTeamId = driver.SeasonTeamId.Value,
+                    RaceId = race.Id,
+                    StrategyId = strategy.Id,
+                });
+            }
+
+            context.AddRange(driverResults);
 
             await context.SaveChangesAsync();
         }
@@ -51,6 +109,56 @@ namespace SimTECH.Data.Services
                     SeasonYear = e.Season.Year
                 })
                 .ToListAsync();
+        }
+
+        public async Task<RaceWeekModel> GetRaceWeekModel(long raceId)
+        {
+            using var context = _dbFactory.CreateDbContext();
+
+            var race = await context.Race
+                .Include(e => e.Penalties)
+                .Include(e => e.Track)
+                    .ThenInclude(e => e.TrackTraits)
+                .SingleAsync(e => e.Id == raceId);
+
+            var strategiesForRace = await context.Strategy
+                .Include(e => e.StrategyTyres)
+                    .ThenInclude(e => e.Tyre)
+                .Where(e => e.StintLength == race.Stints.Count)
+                .ToListAsync();
+
+            var weekendDrivers = await context.Result
+                .Include(e => e.SeasonDriver)
+                    .ThenInclude(e => e.Driver)
+                .Include(e => e.SeasonTeam)
+                .Where(e => e.RaceId == raceId)
+                .Select(result => new RaceWeekDriver
+                {
+                    SeasonDriverId = result.SeasonDriverId,
+                    FullName = result.SeasonDriver.Driver.FullName,
+                    Number = result.SeasonDriver.Number,
+                    Role = result.SeasonDriver.TeamRole,
+                    Country = result.SeasonDriver.Driver.Country,
+                    SeasonTeamId = result.SeasonTeamId,
+                    TeamName = result.SeasonTeam.Name,
+                    Colour = result.SeasonTeam.Colour,
+                    Accent = result.SeasonTeam.Accent,
+                    ResultId = result.Id,
+                    Grid = result.Grid,
+                    Position = result.Position,
+                    Status = result.Status
+                })
+                .ToListAsync();
+
+            if (weekendDrivers?.Any() != true)
+                throw new InvalidOperationException("We're going to need some actual drivers too");
+
+            return new RaceWeekModel
+            {
+                Race = race,
+                RaceWeekDrivers = weekendDrivers,
+                AvailableStrategies = strategiesForRace,
+            };
         }
         #endregion
     }
