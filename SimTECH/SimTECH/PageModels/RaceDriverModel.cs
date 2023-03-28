0596f90fb7981c4c7ef962d7cab7686150f416b6
Mika <mikavandenbrink@outlook.com>
2023-03-28 13:51:13 +0200

    Set a whole bunch stuff, like toolbar headers for most table

---
+++ SimTECH/SimTECH/PageModels/RaceDriverModel.cs
@@ -0,0 +1,58 @@
+﻿using SimTECH.Data.Models;
+
+namespace SimTECH.PageModels
+{
+    public abstract class DriverBase
+    {
+        public long ResultId { get; set; }
+
+        public string FullName { get; set; }
+        public int Number { get; set; }
+        public TeamRole Role { get; set; }
+        public Country Nationality { get; set; }
+
+        public string TeamName { get; set; }
+        public string Colour { get; set; }
+        public string Accent { get; set; }
+
+        public int Power { get; set; }
+
+        public TraitEffect TraitEffect { get; set; }
+    }
+
+    public class RaceDriver
+    {
+        public long ResultId { get; set; }
+        public string FullName { get; set; }
+        public int Number { get; set; }
+        public TeamRole Role { get; set; }
+        public Country Nationality { get; set; }
+        public string TeamName { get; set; }
+        public string Colour { get; set; }
+        public string Accent { get; set; }
+
+        public int Power { get; set; }
+
+        public Result Result { get; set; }//overwegend weg ermee?
+
+        public TraitEffect TraitEffect { get; set; }
+        //score? position?
+        public int[] RunValues { get; set; }
+
+        // Properties underneath are optionable, depending on circumstances
+        public int Position { get; set; }
+        public int Score { get; set; }
+
+        public int MaxScore => RunValues.Max();
+    }
+
+    public class PracticeDriver : DriverBase
+    {
+
+    }
+
+    public class QualifyingDriver : DriverBase
+    {
+
+    }
+}
--- SimTECH/SimTECH/PageModels/RaceModel.cs
+++ SimTECH/SimTECH/PageModels/RaceModel.cs
@@ -12,30 +12,4 @@ namespace SimTECH.PageModels
 
         public List<RaceDriver> RaceDrivers { get; set; }
     }
-
-    public class RaceDriver
-    {
-        public long ResultId { get; set; }
-        public string FullName { get; set; }
-        public int Number { get; set; }
-        public TeamRole Role { get; set; }
-        public Country Nationality { get; set; }
-        public string TeamName { get; set; }
-        public string Colour { get; set; }
-        public string Accent { get; set; }
-
-        public int Power { get; set; }
-
-        public Result Result { get; set; }//overwegend weg ermee?
-
-        public TraitEffect TraitEffect { get; set; }
-        //score? position?
-        public int[] RunValues { get; set; }
-
-        // Properties underneath are optionable, depending on circumstances
-        public int Position { get; set; }
-        public int Score { get; set; }
-
-        public int MaxScore => RunValues.Max();
-    }
 }
