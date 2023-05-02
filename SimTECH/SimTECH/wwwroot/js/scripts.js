867de393df3ca8c1143e6844d647ed4a9ea8fa65
Mika <mikavandenbrink@outlook.com>
2023-05-02 13:53:50 +0200

    Found a version which works!

--- SimTECH/SimTECH/wwwroot/js/scripts.js
+++ SimTECH/SimTECH/wwwroot/js/scripts.js
@@ -1,8 +1,5 @@
 // NOTE: This is not going to work on atleast Firefox since it doesn't allow blobs to be instantly copied to the clipboard
 // Above is true until you set a specific config setting to true, very useful!
-
-// An issue exists where the font isn't loaded, i can't really resolve it either having tried:
-// Explicitly importing and adding font to window, something with nodes svg attachment, 
 window.takeScreenshot = (target) => {
 	var targetElement = document.getElementById(target);
 
@@ -12,7 +9,6 @@ window.takeScreenshot = (target) => {
 				new ClipboardItem({
 					[blob.type]: blob
 				})
-			])
-			.then(() => { console.log("Copied html canvas!"); });
+			]).then(() => { console.log("Copied html canvas!"); });
 		});
 }
