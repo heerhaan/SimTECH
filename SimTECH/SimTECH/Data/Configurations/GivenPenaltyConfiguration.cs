5ce9473b7c50b0449938aa41f51243142a3b7c23
Mika <mikavandenbrink@outlook.com>
2023-05-05 09:38:38 +0200

    Better penalty solution honestly

---
+++ SimTECH/SimTECH/Data/Configurations/GivenPenaltyConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SimTECH.Data.Models;
+
+namespace SimTECH.Data.Configurations
+{
+    public class GivenPenaltyConfiguration : IEntityTypeConfiguration<GivenPenalty>
+    {
+        public void Configure(EntityTypeBuilder<GivenPenalty> builder)
+        {
+            builder.HasKey(e => e.Id);
+        }
+    }
+}
