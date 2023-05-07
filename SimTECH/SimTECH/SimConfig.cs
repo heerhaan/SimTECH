33df04e3db761f578942428207ad10f4b26e5860
Mika <mikavandenbrink@outlook.com>
2023-05-07 11:28:05 +0200

    Resolved some issues with the entrant wizard

--- SimTECH/SimTECH/SimConfig.cs
+++ SimTECH/SimTECH/SimConfig.cs
@@ -7,6 +7,7 @@
         public int FatalityChance { get; set; } = 250;
         public int DisqualifyChance { get; set; } = 100;
         public double GapMarge { get; set; } = 0.05;
+        public bool PersonalNumbers { get; set; } = false;
 
         // MistakeAmountRolls refers to the consequent rolls that need to be done before a driver makes a mistake, mistakes are accounted for in each stint for each driver
         public int MistakeAmountRolls { get; set; } = 1;
