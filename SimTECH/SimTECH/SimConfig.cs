0b7a5c6f91e0b255d876ca9a7ff62264e2e7bd53
Mika <mikavandenbrink@outlook.com>
2023-05-08 11:02:40 +0200

    ei ei ei ik ben zo aardbei

--- SimTECH/SimTECH/SimConfig.cs
+++ SimTECH/SimTECH/SimConfig.cs
@@ -11,10 +11,12 @@
 
         // MistakeAmountRolls refers to the consequent rolls that need to be done before a driver makes a mistake, mistakes are accounted for in each stint for each driver
         public int MistakeAmountRolls { get; set; } = 1;
-        public int MistakeLowerValue { get; set; } = -200;
-        public int MistakeUpperValue { get; set; } = -50;
+        public int MistakeLowerValue { get; set; } = -300;
+        public int MistakeUpperValue { get; set; } = -150;
 
         // Pace modifier a #1 driver gets and a #2 driver loses
         public int CarDriverStatusModifier { get; set; } = 4;
+
+        public int CalculationDistance { get; set; } = 10;
     }
 }
