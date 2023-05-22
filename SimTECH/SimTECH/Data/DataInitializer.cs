6928d701cdde05b6b30bd9edc708b7c89b882441
Mika <mikavandenbrink@outlook.com>
2023-05-22 16:56:01 +0200

    Added some potential default data

--- SimTECH/SimTECH/Data/DataInitializer.cs
+++ SimTECH/SimTECH/Data/DataInitializer.cs
@@ -9,10 +9,10 @@ namespace SimTECH.Data
         {
             var temp = new List<Tyre>
             {
-                new Tyre { Name = "Soft", Colour = "#FFFFFF", Pace = 200, WearMax = -25, WearMin = -15, Length = 10, State = State.Active, },
-                new Tyre { Name = "Medium", Colour = "#FFFFFF", Pace = 180, WearMax = -15, WearMin = -9, Length = 15, State = State.Active, },
-                new Tyre { Name = "Hard", Colour = "#FFFFFF", Pace = 160, WearMax = -10, WearMin = -6, Length = 20, State = State.Active, },
-                new Tyre { Name = "Grooved", Colour = "#FFFFFF", Pace = 60, WearMax = -3, WearMin = -1, Length = 60, State = State.Closed, },
+                new Tyre { Name = "Soft", Colour = "#FFFFFF", Pace = 200, WearMin = 15, WearMax = 25, Length = 10, State = State.Active, },
+                new Tyre { Name = "Medium", Colour = "#FFFFFF", Pace = 180, WearMin = 9, WearMax = 15, Length = 15, State = State.Active, },
+                new Tyre { Name = "Hard", Colour = "#FFFFFF", Pace = 160, WearMin = 6, WearMax = 10, Length = 20, State = State.Active, },
+                new Tyre { Name = "Grooved", Colour = "#FFFFFF", Pace = 60, WearMin = 1, WearMax = 3, Length = 60, State = State.Closed, },
             };
 
             Console.WriteLine(temp);
@@ -54,10 +54,36 @@ namespace SimTECH.Data
             Console.WriteLine(temp);
         }
 
-        public static void SeedTraits() { }
+        public static void SeedTraits()
+        {
+            var temp = new List<Trait>
+            {
+                new Trait { Name = "Rainmeister", Description = "Speed in moist", Type = Entrant.Driver, ForWetConditions = true, QualifyingPace = 3, RacePace = 5, DriverReliability = 3, },
+                new Trait { Name = "Manufacturer", Description = "Owns the engine", Type = Entrant.Team, QualifyingPace = 1, RacePace = 1, EngineReliability = 2, },
+                new Trait { Name = "Street Circuit", Description = "Street is a circuit", Type = Entrant.Track, DriverReliability = -2, CarReliability = -1, WearMax = 2, RngMin = -5, Defense = 10 },
+            };
 
-        public static void SeedTracks() { }
+            Console.WriteLine(temp);
+        }
 
-        public static void SeedManufacturers() { }
+        public static void SeedTracks()
+        {
+            var temp = new List<Track>
+            {
+                new Track { Name = "Spa-Francorchamps", Country = Country.BE, Length = 7.01, AeroMod = 0.55, ChassisMod = 1.1, PowerMod = 1.35, QualifyingMod = 0.7, DefenseMod = 0.9, State = State.Active },
+            };
+
+            Console.WriteLine(temp);
+        }
+
+        public static void SeedManufacturers()
+        {
+            var temp = new List<Manufacturer>
+            {
+                new Manufacturer { Name = "Hankook", Colour = "#0b0b0d", Accent = "#e56103", Pace = 1, WearMin = 0, WearMax = 0, State = State.Active },
+            };
+
+            Console.WriteLine(temp);
+        }
     }
 }
