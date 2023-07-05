7917e29f60f932341ab29c7bc846156de7c567ae
Mika <mikavandenbrink@outlook.com>
2023-07-05 12:08:52 +0200

    Wrote the guide a bit further out

--- SimTECH/SimTECH/SimConfig.cs
+++ SimTECH/SimTECH/SimConfig.cs
@@ -7,18 +7,19 @@
         public int FatalityChance { get; set; } = 250;
         public int DisqualifyChance { get; set; } = 100;
         public int SafetyCarChance { get; set; } = 10;
-        //public int SafetyCarGap { get; set; } = 25;
+        public int SafetyCarGap { get; set; } = 25;
+        public int SafetyPitstopSubtracter { get; set; } = 200;
         public int SafetyCarReturnChance { get; set; } = 2;
-        public int SafetyStopDurationDivider { get; set; } = 2;
-        public double GapMarge { get; set; } = 0.001;
-        public bool PersonalNumbersEnabled { get; set; }
-        public bool FormationLapEnabled { get; set; }
+        public double GapMarge { get; set; } = 0.05;
+        public bool PersonalNumbersEnabled { get; set; } = false;
+        //public bool FormationLapEnabled { get; set; }
 
         // MistakeAmountRolls refers to the consequent rolls that need to be done before a driver makes a mistake, mistakes are accounted for in each stint for each driver
         public int MistakeAmountRolls { get; set; } = 1;
         public int MistakeLowerValue { get; set; } = 150;
         public int MistakeUpperValue { get; set; } = 300;
         public int BattleRng { get; set; } = 10;
+        public int MinimumTyreLife { get; set; } = -25;
 
         // Pace modifier a #1 driver gets and a #2 driver loses
         public int CarDriverStatusModifier { get; set; } = 4;
