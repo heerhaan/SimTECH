f8dfdc61b6367c24ae54b98598a476980d5b91b3
Mika <mikavandenbrink@outlook.com>
2023-07-06 18:15:47 +0200

    did stuff (and thangs)

--- SimTECH/SimTECH/PageModels/FinishedRaceModel.cs
+++ SimTECH/SimTECH/PageModels/FinishedRaceModel.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public int Round { get; set; }
         public Country Country { get; set; }
+        public string SeasonDisplay { get; set; }
         public string LeagueName { get; set; }
 
         public CompactDriver WinningDriver { get; set; }
