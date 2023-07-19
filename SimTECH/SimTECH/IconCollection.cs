a0409eeef25a6c5a1926901af1314b2e43d2dfa6
Mika <mikavandenbrink@outlook.com>
2023-07-19 09:21:37 +0200

    motif avec une shoguns clan

--- SimTECH/SimTECH/IconCollection.cs
+++ SimTECH/SimTECH/IconCollection.cs
@@ -312,5 +312,12 @@
         </svg>";
         // Y
         // Z
+
+        // Specials
+        public static string CubePurpleLayer => @"<svg xmlns=""http://www.w3.org/2000/svg"" version=""1.1"" xmlns:xlink=""http://www.w3.org/1999/xlink"" xmlns:svgjs=""http://svgjs.dev/svgjs"" viewBox=""0 0 800 800"" width=""800"" height=""800""><defs><pattern id=""mmmotif-pattern"" width=""40"" height=""40"" patternUnits=""userSpaceOnUse"" patternTransform=""translate(-44 -32) scale(1) rotate(134) skewX(0) skewY(0)"">
+            <rect width=""18"" height=""18"" transform=""matrix(0.866025 0.5 -0.866025 0.5 20 2)"" fill=""hsl(272, 99%, 54%)""></rect>
+            <rect width=""18"" height=""18"" transform=""matrix(0.866025 0.5 -2.20305e-08 1 4.41162 11)"" fill=""#c956ff""></rect>
+            <rect width=""18"" height=""18"" transform=""matrix(0.866025 -0.5 2.20305e-08 1 20 20)"" fill=""#5700c9""></rect>
+        </pattern></defs><rect width=""800"" height=""800"" fill=""url(#mmmotif-pattern)""></rect></svg>";
     }
 }
