6952c73c9db8ce5ff6c6541eab115ff15e0295d2
Mika <mikavandenbrink@outlook.com>
2023-08-06 15:44:57 +0200

    Resolve various issues at raceweek, improve implementation there but we arent done yet withthis bich

---
+++ SimTECH/SimTECH/PageModels/Racing/PracticeSession.cs
@@ -0,0 +1,9 @@
+﻿using SimTECH.Data.Models;
+
+namespace SimTECH.PageModels.Racing
+{
+    public class PracticeSession : SessionBase
+    {
+        public List<PracticeScore> SessionScores { get; set; }
+    }
+}
