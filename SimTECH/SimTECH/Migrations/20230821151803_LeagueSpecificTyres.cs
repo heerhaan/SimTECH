bdd9e29821cea802e46c12a1644540b86f4455e2
Mika <mikavandenbrink@outlook.com>
2023-08-25 17:46:05 +0200

    commented out part of the migration that did an ouchie

--- SimTECH/SimTECH/Migrations/20230821151803_LeagueSpecificTyres.cs
+++ SimTECH/SimTECH/Migrations/20230821151803_LeagueSpecificTyres.cs
@@ -36,18 +36,18 @@ namespace SimTECH.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            migrationBuilder.InsertData(
-                table: "Track",
-                columns: new[] { "Id", "AeroMod", "ChassisMod", "Country", "DefenseMod", "Length", "Name", "PowerMod", "QualifyingMod", "State" },
-                values: new object[,]
-                {
-                    { 3L, 0.84999999999999998, 1.05, 32, 0.80000000000000004, 4.3099999999999996, "Autodromo de Interlagos", 1.1000000000000001, 0.90000000000000002, 1 },
-                    { 4L, 0.94999999999999996, 1.1000000000000001, 157, 1.3, 4.5499999999999998, "TT Assen", 0.94999999999999996, 1.1000000000000001, 1 },
-                    { 5L, 1.05, 0.90000000000000002, 112, 0.66000000000000003, 5.9900000000000002, "Fuji Speedway", 1.05, 0.90000000000000002, 1 },
-                    { 6L, 1.05, 0.94999999999999996, 15, 1.0, 4.3300000000000001, "Österreichring", 1.1000000000000001, 0.80000000000000004, 1 },
-                    { 7L, 0.80000000000000004, 0.75, 110, 1.2, 5.79, "Autodromo Nazionale di Monza", 1.25, 1.2, 1 },
-                    { 8L, 1.1000000000000001, 0.90000000000000002, 145, 0.90000000000000002, 5.54, "Sepang", 1.1000000000000001, 1.0, 1 }
-                });
+            //migrationBuilder.InsertData(
+            //    table: "Track",
+            //    columns: new[] { "Id", "AeroMod", "ChassisMod", "Country", "DefenseMod", "Length", "Name", "PowerMod", "QualifyingMod", "State" },
+            //    values: new object[,]
+            //    {
+            //        { 3L, 0.84999999999999998, 1.05, 32, 0.80000000000000004, 4.3099999999999996, "Autodromo de Interlagos", 1.1000000000000001, 0.90000000000000002, 1 },
+            //        { 4L, 0.94999999999999996, 1.1000000000000001, 157, 1.3, 4.5499999999999998, "TT Assen", 0.94999999999999996, 1.1000000000000001, 1 },
+            //        { 5L, 1.05, 0.90000000000000002, 112, 0.66000000000000003, 5.9900000000000002, "Fuji Speedway", 1.05, 0.90000000000000002, 1 },
+            //        { 6L, 1.05, 0.94999999999999996, 15, 1.0, 4.3300000000000001, "Österreichring", 1.1000000000000001, 0.80000000000000004, 1 },
+            //        { 7L, 0.80000000000000004, 0.75, 110, 1.2, 5.79, "Autodromo Nazionale di Monza", 1.25, 1.2, 1 },
+            //        { 8L, 1.1000000000000001, 0.90000000000000002, 145, 0.90000000000000002, 5.54, "Sepang", 1.1000000000000001, 1.0, 1 }
+            //    });
 
             migrationBuilder.CreateIndex(
                 name: "IX_LeagueTyre_TyreId",
@@ -61,35 +61,35 @@ namespace SimTECH.Migrations
             migrationBuilder.DropTable(
                 name: "LeagueTyre");
 
-            migrationBuilder.DeleteData(
-                table: "Track",
-                keyColumn: "Id",
-                keyValue: 3L);
+            //migrationBuilder.DeleteData(
+            //    table: "Track",
+            //    keyColumn: "Id",
+            //    keyValue: 3L);
 
-            migrationBuilder.DeleteData(
-                table: "Track",
-                keyColumn: "Id",
-                keyValue: 4L);
+            //migrationBuilder.DeleteData(
+            //    table: "Track",
+            //    keyColumn: "Id",
+            //    keyValue: 4L);
 
-            migrationBuilder.DeleteData(
-                table: "Track",
-                keyColumn: "Id",
-                keyValue: 5L);
+            //migrationBuilder.DeleteData(
+            //    table: "Track",
+            //    keyColumn: "Id",
+            //    keyValue: 5L);
 
-            migrationBuilder.DeleteData(
-                table: "Track",
-                keyColumn: "Id",
-                keyValue: 6L);
+            //migrationBuilder.DeleteData(
+            //    table: "Track",
+            //    keyColumn: "Id",
+            //    keyValue: 6L);
 
-            migrationBuilder.DeleteData(
-                table: "Track",
-                keyColumn: "Id",
-                keyValue: 7L);
+            //migrationBuilder.DeleteData(
+            //    table: "Track",
+            //    keyColumn: "Id",
+            //    keyValue: 7L);
 
-            migrationBuilder.DeleteData(
-                table: "Track",
-                keyColumn: "Id",
-                keyValue: 8L);
+            //migrationBuilder.DeleteData(
+            //    table: "Track",
+            //    keyColumn: "Id",
+            //    keyValue: 8L);
         }
     }
 }
