1ac842b4689ea423aa0bdc8a44b01ae6c516f816
Mika van den Brink <mikavandenbrink@outlook.com>
2023-09-18 15:36:13 +0200

    It is what it is

--- SimTECH/SimTECH/PageModels/SeasonModels/OverviewModel.cs
+++ SimTECH/SimTECH/PageModels/SeasonModels/OverviewModel.cs
@@ -6,5 +6,10 @@ public class OverviewModel
 {
     public Season Season { get; set; }
 
+    public List<Race> Races { get; set; }
+    public List<SeasonDriver> SeasonDrivers { get; set; }
+    public List<SeasonTeam> SeasonTeams { get; set; }
+    public List<SeasonEngine> SeasonEngines { get; set; }
+
     public long ActiveClassId { get; set; }
 }
