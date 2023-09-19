6871325fa8a5353f3de258703bd1758f8f912c72
Mika <mikavandenbrink@outlook.com>
2023-09-19 19:42:11 +0200

    Ok NOW is overview fully done, this is a great solution

---
+++ SimTECH/SimTECH/PageModels/SeasonModels/PowerRankingItem.cs
@@ -0,0 +1,34 @@
+﻿namespace SimTECH.PageModels.SeasonModels
+{
+    public class PowerRankingItem
+    {
+        public string DriverName { get; set; }
+        public int DriverNumber { get; set; }
+        public Country Nationality { get; set; }
+
+        public string TeamName { get; set; }
+        public string Colour { get; set; } = Constants.DefaultColour;
+        public string Accent { get; set; } = Constants.DefaultAccent;
+
+        public string Engine { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public int QualyPower { get; set; }
+        public int RacePower { get; set; }
+        public int Aero { get; set; }
+        public int Chassis { get; set; }
+        public int Powertrain { get; set; }
+        public int DriverRel { get; set; }
+        public int CarRel { get; set; }
+        public int EngineRel { get; set; }
+        public int LifeBonus { get; set; }
+        public int WearMin { get; set; }
+        public int WearMax { get; set; }
+        public int RngMin { get; set; }
+        public int RngMax { get; set; }
+
+        public double CarPartAvg { get; set; }
+        public double RelAvg { get; set; }
+    }
+}
