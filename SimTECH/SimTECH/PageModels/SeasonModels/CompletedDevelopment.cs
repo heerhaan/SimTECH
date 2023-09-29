c40742cca996cbb5200a10fb0db73436bfeac951
Mika <mikavandenbrink@outlook.com>
2023-09-29 18:10:48 +0200

    well at least it looks better now

--- SimTECH/SimTECH/PageModels/SeasonModels/CompletedDevelopment.cs
+++ SimTECH/SimTECH/PageModels/SeasonModels/CompletedDevelopment.cs
@@ -1,25 +1,5 @@
-﻿using SimTECH.Data.Models;
-
-namespace SimTECH.PageModels.SeasonModels
+﻿namespace SimTECH.PageModels.SeasonModels
 {
-    public class CompletedDevelopment
-    {
-        public CompletedDevelopment(Entrant group, Aspect aspect, int min, int max, List<DevelopedEntrant> entrants)
-        {
-            EntrantGroup = group;
-            DevelopedAspect = aspect;
-            MinChange = min;
-            MaxChange = max;
-            Entrants = entrants?.ToArray() ?? Array.Empty<DevelopedEntrant>();
-        }
-
-        public Entrant EntrantGroup { get; set; }
-        public Aspect DevelopedAspect { get; set; }
-        public int MinChange { get; set; }
-        public int MaxChange { get; set; }
-        public DevelopedEntrant[] Entrants { get; set; }
-    }
-
     public class DevelopSummary
     {
         public Entrant EntrantGroup { get; set; }
