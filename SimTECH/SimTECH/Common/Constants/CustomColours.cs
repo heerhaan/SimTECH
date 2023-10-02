ad39afb412905f80116a9d5ea089dd3f3dc3ed0b
Mika van den Brink <mikavandenbrink@outlook.com>
2023-10-02 16:47:06 +0200

    wowie cyberpunk theme

--- SimTECH/SimTECH/Common/Constants/CustomColours.cs
+++ SimTECH/SimTECH/Common/Constants/CustomColours.cs
@@ -7,6 +7,10 @@ public static class CustomColours
     public static readonly MudColor UltraDarkFadedRed = new("#100a0a");
     public static readonly MudColor DeepDarkFadedRed = new("#110808");
     public static readonly MudColor DarkFadedRed = new("#250e0e");
+    public static readonly MudColor DarkFadedRedAlt = new("#31181e");
+    public static readonly MudColor MaroonRed = new("#B12926");
+    public static readonly MudColor ActiveDarkRed = new("#3C0D0F");
+    public static readonly MudColor FadedRed = new("#FF463F");
     public static readonly MudColor BrightRed = new("#ff0015");
 
     public static readonly MudColor LightBeige = new("#ffac79");
@@ -22,10 +26,15 @@ public static class CustomColours
     public static readonly MudColor DarkFadedPurple = new("#19141f");
     public static readonly MudColor LiteDarkFadedPurple = new("#140a24");
 
+    public static readonly MudColor UltraDeepDarkFadedBlue = new("#040508");
     public static readonly MudColor UltraDarkFadedBlue = new("#060f14");
     public static readonly MudColor DeepDarkFadedBlue = new("#09131a");
     public static readonly MudColor DarkFadedBlue = new("#0c1e27");
     public static readonly MudColor LiteDarkFadedBlue = new("#122d3b");
+
+    public static readonly MudColor BrightCyan = new("#4CF4FF");
+
+    public static readonly MudColor StreetGreen = new("#18B256");
 
     public static readonly MudColor DarkBlack = new("#0f0f0f");
     public static readonly MudColor DeepDarkBlack = new("#070707");
