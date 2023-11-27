0765e5fd6eb283d508e04cbb574cdf324d062595
Mika van den Brink <mikavandenbrink@outlook.com>
2023-11-27 18:08:13 +0100

    Began with refactoring trait assigner page

--- SimTECH/SimTECH/PageModels/EntrantAssignee.cs
+++ SimTECH/SimTECH/PageModels/EntrantAssignee.cs
@@ -50,6 +50,9 @@ public class EntrantAssignee
     public Country Country { get; set; }
     public Entrant Entrant { get; set; }
 
-    public List<long> ExistingTraitIds { get; set; } = new();
-    public List<long> AssignedTraitIds { get; set; } = new();
+    public List<long> ExistingTraitIds { get; set; } = [];
+    public List<long> AssignedTraitIds { get; set; } = [];
+
+    public Dictionary<long, string> ExistingTraits { get; set; } = [];
+    public Dictionary<long, string> AssignedTraits { get; set; } = [];
 }
---
+++ SimTECH/SimTECH/PageModels/Traits/AssignableTrait.cs
@@ -0,0 +1,8 @@
+﻿namespace SimTECH.PageModels.Traits;
+
+public class AssignableTrait
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public bool IsSet { get; set; }
+}
