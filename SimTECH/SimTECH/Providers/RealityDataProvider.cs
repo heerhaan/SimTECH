ad4e2e8114b30a9d08bfd13af21421d2641c4701
Mika van den Brink <mikavandenbrink@outlook.com>
2023-12-05 10:42:57 +0100

    Got a magnificent idea related to reality data providing

---
+++ SimTECH/SimTECH/Providers/RealityDataProvider.cs
@@ -0,0 +1,24 @@
+﻿using MudBlazor;
+using SimTECH.Common.Enums;
+using SimTECH.Constants;
+using SimTECH.Data.Models;
+using SimTECH.Extensions;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace SimTECH.Providers;
+
+public class RealityDataProvider
+{
+    private readonly IWebHostEnvironment _env;
+    private readonly string[] dataFiles;
+
+    public RealityDataProvider(IWebHostEnvironment env)
+    {
+        _env = env;
+        dataFiles = Directory.GetFiles(Path.Combine(_env.WebRootPath, "csv"));
+    }
+
+
+}
