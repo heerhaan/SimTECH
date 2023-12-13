c5846ea3d69f796a4d7bf191dfae95931b34e88b
Mika <mikavandenbrink@outlook.com>
2023-12-13 23:27:28 +0100

    Heeelp?? Help!

---
+++ SimTECH/SimTECH/Providers/GuideBookProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace SimTECH.Providers;
+
+public static class GuideBookProvider
+{
+    private static readonly Dictionary<Type, Type> pageGuidanceDict = new()
+    {
+        { typeof(Pages.Index), typeof(Pages.Guide.Topics.Intro) }
+    };
+
+    public static Type GetHelpContentForPage(Type page)
+    {
+        if (pageGuidanceDict.TryGetValue(page, out Type? value))
+            return value;
+
+        return typeof(Pages.Guide.Topics.UnknownTopic);
+    }
+}
