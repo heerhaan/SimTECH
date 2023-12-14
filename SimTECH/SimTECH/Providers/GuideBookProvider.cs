8eb0af5fe54f275b5382e57358c161e5f1a9e207
Mika van den Brink <mikavandenbrink@outlook.com>
2023-12-14 18:05:44 +0100

    Moved the help content to the provider defined area

--- SimTECH/SimTECH/Providers/GuideBookProvider.cs
+++ SimTECH/SimTECH/Providers/GuideBookProvider.cs
@@ -1,10 +1,25 @@
-﻿namespace SimTECH.Providers;
+﻿using SimTECH.Shared.GuideItems;
+
+namespace SimTECH.Providers;
 
 public static class GuideBookProvider
 {
     private static readonly Dictionary<Type, Type> pageGuidanceDict = new()
     {
-        { typeof(Pages.Index), typeof(Pages.Guide.Topics.Intro) }
+        { typeof(Pages.Index), typeof(Pages.Guide.Topics.Intro) },
+        { typeof(Pages.Climates.Index), typeof(ClimateTopic) },
+        { typeof(Pages.Leagues.ContractManager.Index), typeof(ContractTopic) },
+        { typeof(Pages.Seasons.Overview.Development.Index), typeof(DevelopTopic) },
+        { typeof(Pages.Entrants.Drivers.Index), typeof(DriverTopic) },
+        { typeof(Pages.Entrants.Engines.Index), typeof(EngineTopic) },
+        { typeof(Pages.Incidents.Index), typeof(IncidentTopic) },
+        { typeof(Pages.Leagues.Index), typeof(LeagueTopic) },
+        { typeof(Pages.Manufacturers.Index), typeof(ManufacturerTopic) },
+        { typeof(Pages.Seasons.Index), typeof(SeasonTopic) },
+        { typeof(Pages.Entrants.Teams.Index), typeof(TeamTopic) },
+        { typeof(Pages.Entrants.Tracks.Index), typeof(TrackTopic) },
+        { typeof(Pages.Traits.Index), typeof(TraitTopic) },
+        { typeof(Pages.Tyres.Index), typeof(TyreTopic) },
     };
 
     public static Type GetHelpContentForPage(Type page)
@@ -12,6 +27,6 @@ public static class GuideBookProvider
         if (pageGuidanceDict.TryGetValue(page, out Type? value))
             return value;
 
-        return typeof(Pages.Guide.Topics.UnknownTopic);
+        return typeof(NotFoundTopic);
     }
 }
