befc39d660d12e81501bbf45d305457f1b5bccb1
Mika van den Brink <mikavandenbrink@outlook.com>
2024-03-21 18:16:37 +0100

    Bug fix for the entrant wizard

---
+++ SimTECH/SimTECH/Common/Enums/EntrantWizardStepEnumHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace SimTECH.Common.Enums;
+
+public enum EntrantWizardStep
+{
+    EnginesAdd = 0,
+    EnginesEdit = 1,
+    TeamsAdd = 2,
+    TeamsEdit = 3,
+    TeamsAssignment = 4,
+    RaceClassAssignment = 5,
+    DriversAdd = 6,
+    DriversEdit = 7,
+    DriversAssignment = 8,
+    Summary = 9,
+}
+
+public static class EntrantWizardStepEnumHelper
+{
+    public static string GetStepTitle(this EntrantWizardStep step) => step switch
+    {
+        EntrantWizardStep.EnginesAdd => "Add engines",
+        EntrantWizardStep.EnginesEdit => "Edit engines",
+        EntrantWizardStep.TeamsAdd => "Add teams",
+        EntrantWizardStep.TeamsEdit => "Edit teams",
+        EntrantWizardStep.TeamsAssignment => "Assign teams",
+        EntrantWizardStep.RaceClassAssignment => "Assign race classes",
+        EntrantWizardStep.DriversAdd => "Add drivers",
+        EntrantWizardStep.DriversEdit => "Edit drivers",
+        EntrantWizardStep.DriversAssignment => "Assign drivers",
+        EntrantWizardStep.Summary => "Participants",
+        _ => "Unknown (how tf did you do this)",
+    };
+}
