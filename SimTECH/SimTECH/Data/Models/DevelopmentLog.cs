23f3c4695412d04bf0d5e8ecd1610669b7625ec3
Mika van den Brink <mikavandenbrink@outlook.com>
2024-04-04 17:10:05 +0200

    Migration and addition for Initial Number at develop log

--- SimTECH/SimTECH/Data/Models/DevelopmentLog.cs
+++ SimTECH/SimTECH/Data/Models/DevelopmentLog.cs
@@ -1,19 +1,19 @@
 ﻿using SimTECH.Common.Enums;
 
-namespace SimTECH.Data.Models
+namespace SimTECH.Data.Models;
+
+public sealed class DevelopmentLog : ModelBase
 {
-    public sealed class DevelopmentLog : ModelBase
-    {
-        public long EntrantId { get; set; }
-        public Entrant EntrantGroup { get; set; }
-        public Aspect DevelopedAspect { get; set; }
+    public long EntrantId { get; set; }
+    public Entrant EntrantGroup { get; set; }
+    public Aspect DevelopedAspect { get; set; }
 
-        //public int Initial { get; set; }
-        public int Change { get; set; }
+    // INFO: Initial was added later than other props here, keep in mind for existing data
+    public int Initial { get; set; }
+    public int Change { get; set; }
 
-        public int AfterRound { get; set; }
+    public int AfterRound { get; set; }
 
-        public long SeasonId { get; set; }
-        public Season Season { get; set; }
-    }
+    public long SeasonId { get; set; }
+    public Season Season { get; set; }
 }
