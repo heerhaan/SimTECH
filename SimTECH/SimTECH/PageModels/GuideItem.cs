6b40147ea9a66ef644eaca44bca09b4cad35c7a5
Mika van den Brink <mikavandenbrink@outlook.com>
2024-04-16 18:14:34 +0200

    Supporting further guide changes 'ere

---
+++ SimTECH/SimTECH/PageModels/GuideItem.cs
@@ -0,0 +1,26 @@
+﻿using MudBlazor;
+using SimTECH.Constants;
+
+namespace SimTECH.PageModels;
+
+public class GuideItem
+{
+    public string Link { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Icon { get; set; } = IconCollection.Pyramid;
+
+    public int Order { get; set; }
+}
+
+public class GuideGroupItem : GuideItem
+{
+    public bool Expanded { get; set; } = false;
+
+    public string ExpandedIcon { get; set; } = Icons.Custom.Uncategorized.FolderOpen;
+
+    public List<GuideItem> Children { get; set; } = [];
+
+    public bool HasChildren => Children.Count != 0;
+}
--- SimTECH/SimTECH/PageModels/Seasons/Development/DevelopModel.cs
+++ SimTECH/SimTECH/PageModels/Seasons/Development/DevelopModel.cs
@@ -5,11 +5,14 @@ namespace SimTECH.PageModels.Seasons.Development;
 public class DevelopModel
 {
     public long ActiveRaceClassId { get; set; }
+
     public Entrant ActiveEntrant { get; set; } = Entrant.Driver;
     public Aspect ActiveAspect { get; set; } = Aspect.Reliability;
-    public Quantifier ActiveQuantifier { get; set; } = Quantifier.Range;
+    public Quantifier ActiveQuantifier { get; set; } = Quantifier.Set;
+
     public int MinChange { get; set; }
     public int MaxChange { get; set; }
     public bool IsOptionalColumnVisible { get; set; }
+
     public bool IsDirectRangeReadOnly => ActiveQuantifier != Quantifier.Direct;
 }
