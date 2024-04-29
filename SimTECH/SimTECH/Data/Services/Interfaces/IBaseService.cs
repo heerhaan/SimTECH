a81bd0aaceb092362b3d3b5ab42010862f04f169
Mika van den Brink <mikavandenbrink@outlook.com>
2024-04-29 17:32:16 +0200

    Add interfaces for the services

---
+++ SimTECH/SimTECH/Data/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using SimTECH.Data.Models;
+
+namespace SimTECH.Data.Services.Interfaces;
+
+public interface IBaseService<T> where T : ModelBase
+{
+    public Task<T> GetItemById(long id);
+
+    public Task BulkAddItems(List<T> items);
+}
---
+++ SimTECH/SimTECH/Data/Services/Interfaces/IStateService.cs
@@ -0,0 +1,11 @@
+﻿using SimTECH.Common.Enums;
+using SimTECH.Data.Models;
+
+namespace SimTECH.Data.Services.Interfaces;
+
+public interface IStateService<T> : IBaseService<T> where T : ModelState
+{
+    public Task ChangeState(T item, State targetState);
+
+    public Task ArchiveItem(T item);
+}
